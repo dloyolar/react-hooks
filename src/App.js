08a1fb00c82565385d03fa58253f114206866083
dloyolar <dieegolr@gmail.com>
2022-01-31 22:35:55 -0300

    [ADD] Add darkmode

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
+import { useState } from 'react';
 import './App.css';
 import { Characters } from './components/Characters';
 import { Header } from './components/Header';
+import { ThemeContext } from './context/ThemeContext';
 
 function App() {
+  const [theme, setTheme] = useState('bg-light');
+
   return (
-    <div className="App">
-      <Header />
-      <Characters />
-      <h1>Hola Mundo</h1>
-    </div>
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      <div className={('App', theme)}>
+        <Header />
+        <Characters />
+        <h1>Hola Mundo</h1>
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,15 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
+import { ThemeContext } from '../context/ThemeContext';
+
+import '../styles/Header.css';
 
 export const Header = () => {
+  const { theme, setTheme } = useContext(ThemeContext);
   const [darkMode, setDarkMode] = useState(false);
 
   const handleClick = () => {
     setDarkMode(!darkMode);
+    theme === 'bg-light' ? setTheme('bg-dark') : setTheme('bg-light');
   };
 
   return (
